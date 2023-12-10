6d095c769476fe81703e383d588596770d18aa9c
Davi Júlio <davijulio550@gmail.com>
2023-12-09 22:43:36 -0300

    Alteração no banco de dados

--- Model/config.php
+++ Model/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$localhost = 'localhost';
-$user ='root';
-$pass ='';
-$db_name = 'users';
+$localhost = 'sql106.infinityfree.com';
+$user ='if0_35589871';
+$pass ='YLnm6PBa2Pq';
+$db_name = 'if0_35589871_login';
 
 $mysqli = new mysqli($localhost,$user,$pass,$db_name);
 
