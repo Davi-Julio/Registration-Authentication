4501ae9603327cfc5b6651dafadfabfcb3553d40
Davi Júlio <davijulio550@gmail.com>
2023-12-22 22:42:31 -0300

    db

--- Model/config.php
+++ Model/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$localhost = 'sql106.infinityfree.com';
-$user ='if0_35589871';
-$pass ='YLnm6PBa2Pq';
-$db_name = 'if0_35589871_login';
+$localhost = 'localhost';
+$user ='root';
+$pass ='';
+$db_name = 'users';
 
 $mysqli = new mysqli($localhost,$user,$pass,$db_name);
 
